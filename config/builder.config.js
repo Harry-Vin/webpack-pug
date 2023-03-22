3304fb9b18d5e491c60364130bd4236c5b970afb
HarryVin <28012528@qq.com>
2023-03-22 23:50:56 +0800

    工程优化

--- config/builder.config.js
+++ config/builder.config.js
@@ -1,17 +1,17 @@
 const path = require("path");
 
-exports.PORT = 3000;
+exports.PORT = 3000; // 开发环境端口号
 exports.PATH = {
-    PUBLIC_PATH: "/",
-    PUBLIC_PATH_DEV: "/",
-    ROOT: path.resolve(__dirname,"..")
+    PUBLIC_PATH: "/", // 打包的服务器资源目录路径
+    PUBLIC_PATH_DEV: "/", //开发时的服务器资源路径（一般情况下无需改动）
+    ROOT: path.resolve(__dirname,"..") //当前项目根目录（一般情况下无需改动）
 }
 exports.DIR = {
-    DIST: 'dist',
-    STYLE: "styles",
-    SCRIPT: "js",
-    FONT: "fonts",
-    IMAGE: "images",
-    PUG: 'pug',
-    WORK: 'src'
+    DIST: 'dist', // 打包输出目录
+    STYLE: "styles", //打包输出样式文件目录
+    SCRIPT: "js", //打包输出js文件目录
+    FONT: "fonts", //打包输出字体文件目录
+    IMAGE: "images", //打包输出图片文件目录
+    PUG: 'pug', //pug文件目录名（一般情况下无需改动）
+    WORK: 'src' // 主要工作目录 （一般情况下无需改动）
 }
