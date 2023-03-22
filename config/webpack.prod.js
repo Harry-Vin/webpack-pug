c9b7c48f151e41f12b8b661bda8dbd711ec822e3
HarryVin <28012528@qq.com>
2023-03-22 22:34:25 +0800

    去掉开发依赖暴露所有依赖

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,8 +2,7 @@ const marge = require('webpack-merge').merge;
 const baseConfig = require('./webpack.config');
 
 module.exports = marge(baseConfig, {
-    mode: 'production',
-    devtool: "source-map"
+    mode: 'production'
 })
 
 
